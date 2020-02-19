c0b8a0201196d59212c0002d246c293ba8cb9023
Devesh <mailme.devmehta@gmail.com>
2020-02-19 18:12:56 +0530

    attendee listing

--- src/app/client/src/app/staff/components/dashboard/dashboard.component.ts
+++ src/app/client/src/app/staff/components/dashboard/dashboard.component.ts
@@ -4,8 +4,8 @@ import {map} from 'rxjs/operators';
 import {DataService} from '../../service/data.service';
 import {Router, ActivatedRoute} from '@angular/router';
 import {ToasterService} from '@sunbird/shared';
-import {forkJoin} from 'rxjs';
-import { ConfigService } from '../../../config/config.service';
+import {forkJoin, observable} from 'rxjs';
+import {ConfigService} from '../../../config/config.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -20,6 +20,25 @@ export class DashboardComponent implements OnInit {
   queryParam: any;
   timeTable: any;
   loggedIn = false;
+  attendenceList = [
+    {
+      'eid': 'DC_ATTEND',
+      'ets': 1.582108972974E12,
+      'did': '5951a18697b3fd79cdaade314c68d8872f70cfd7',
+      'profileId': '1-c3c0b911-5028-4d6b-b525-9ee943eac0cd',
+      'stallId': 'STA2',
+      'ideaId': 'IDE9',
+      'sid': '08021717',
+      'edata': {
+        'profileUrl': 'https://devcon2020.blob.core.windows.net/user/profile/File-01296063808148275285.png',
+        'name': 'Test',
+        'osid': '1-c3c0b911-5028-4d6b-b525-9ee943eac0cd'
+      },
+      'syncts': 1582109022835,
+      '@timestamp': '2020-02-19T10:43:42.835Z'
+    }
+  ];
+
   constructor(private dataService: DataService, public activatedRoute: ActivatedRoute,
               public toasterService: ToasterService, public configService: ConfigService) {
                 if(this.configService.userInfo){
@@ -27,13 +46,14 @@ export class DashboardComponent implements OnInit {
                   console.log('===user loggedin in constructor=====', this.configService.userInfo, this.configService.teacherInfo);
                 }
   }
-  handleLogin(){
+
+  handleLogin() {
     this.loggedIn = true;
     console.log('===user loggedin event=====', this.configService.userInfo, this.configService.teacherInfo);
   }
   ngOnInit() {
-    if(!this.loggedIn){
-      console.log("not logged in returning in ngoninit");
+    if (!this.loggedIn) {
+      console.log('not logged in returning in ngoninit');
       return;
     }
     this.fetchData(this.activatedRoute.snapshot.params.qrCode).subscribe(data => {
@@ -84,6 +104,18 @@ export class DashboardComponent implements OnInit {
       }
     }).pipe(map((res: any) => {
       return res.result.Period;
+    }));
+  }
+
+  getAttendence(sessionId) {
+    return this.dataService.getData('https://devcon.sunbirded.org/api/teacher/v3/user/profile/ABC123415', {
+      headers: {
+        'Content-Type': 'application/json',
+        // tslint:disable-next-line:max-line-length
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIyZWU4YTgxNDNiZWE0NDU4YjQxMjcyNTU5ZDBhNTczMiJ9.7m4mIUaiPwh_o9cvJuyZuGrOdkfh0Nm0E_25Cl21kxE'
+      }
+    }).pipe(map((res: any) => {
+      return res.result.attendenceList;
     }));
   }
 
