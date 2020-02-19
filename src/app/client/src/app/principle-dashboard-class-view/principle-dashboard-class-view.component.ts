f99004c1bcf6d95165ccd17b41c04b7f583b027a
Arjun Khetia <khetia.jagdish@stackroute.in>
2020-02-20 01:28:56 +0530

    Fix: Assessment + Attendence Done

--- src/app/client/src/app/principle-dashboard-class-view/principle-dashboard-class-view.component.ts
+++ src/app/client/src/app/principle-dashboard-class-view/principle-dashboard-class-view.component.ts
@@ -2,33 +2,58 @@ import { Component, OnInit } from '@angular/core';
 import {SLIDE_UP_DOWN, FLYIN, APPEAR_DOWN, CARD_ANIMATION, APPEAR_SIDE} from '../theme/app-animations';
 import {DataService} from './../staff/service/data.service';
 import {map, mergeMap} from 'rxjs/operators';
-import * as _ from "lodash";
+import * as _ from 'lodash';
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-principle-dashboard-class-view',
   templateUrl: './principle-dashboard-class-view.component.html',
   styleUrls: ['./principle-dashboard-class-view.component.scss'],
-  animations: [SLIDE_UP_DOWN, FLYIN, APPEAR_DOWN, CARD_ANIMATION, APPEAR_SIDE]
+  animations: [SLIDE_UP_DOWN, FLYIN, APPEAR_DOWN, CARD_ANIMATION, APPEAR_SIDE,
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotate(0)' })),
+      state('rotated', style({ transform: 'rotate(-360deg)' })),
+      transition('default => rotated', animate('1500ms ease-in'))
+  ])]
 })
 export class PrincipleDashboardClassViewComponent implements OnInit {
 
+  curentSessionId: any;
   constructor(private dataService: DataService,) { }
   timeTableList = [];
+  attendenceList: any;
+  assessmentScore: any;
+  state = 'default';
+  state2 = 'default';
   periodToProfileMap = {};
   ngOnInit() {
-    this.populateTimeTable("08");
+    this.populateTimeTable('08');
+    this.getAttendence(this.curentSessionId).subscribe(data => {
+      this.attendenceList = data;
+    });
+    this.getAssessmentScore(this.curentSessionId).subscribe(data => {
+      this.assessmentScore = data;
+    });
   }
   handleClassChange(event){
-    console.log("class", event.target.value);
+    console.log('class', event.target.value);
+    const date = new Date();
+    const dt = date.getDate();
+    let hh: any = date.getHours();
+    if (hh <= 9) {
+      hh = '0' + hh;
+    }
+    this.curentSessionId = event.target.value + '02' + dt + hh;
+    console.log(this.curentSessionId);
     this.populateTimeTable(event.target.value);
   }
   getProfileData(){
     const body = {
-      "request": {
-          "filters": {
-            "objectType":"Teacher",
-            "identifier": ["T1", "T2","T3", "T4", "T5","T6"],
-            "status": []
+      'request': {
+          'filters': {
+            'objectType':'Teacher',
+            'identifier': ['T1', 'T2','T3', 'T4', 'T5','T6'],
+            'status': []
           }
       }
     };
@@ -44,7 +69,7 @@ export class PrincipleDashboardClassViewComponent implements OnInit {
   getTimeTableForClass(classId){
     const date = new Date();
     const day = date.getDate();
-    const month = "0" + (date.getMonth() + 1);
+    const month = '0' + (date.getMonth() + 1);
     return this.dataService.post('https://devcon.sunbirded.org/action/composite/v3/search', {
       'request': {
         'filters': {
@@ -67,7 +92,7 @@ export class PrincipleDashboardClassViewComponent implements OnInit {
   }
   populateTimeTable(classId) {
     this.getProfileData().pipe(mergeMap((profileData) => {
-      console.log("==profileData==", profileData);
+      console.log('==profileData==', profileData);
       return this.getTimeTableForClass(classId)
     }))
     .subscribe(timeTableOfClass => {
@@ -82,14 +107,59 @@ export class PrincipleDashboardClassViewComponent implements OnInit {
           day: parseInt(sessionIdArray[2]),
           hour: parseInt(sessionIdArray[3])
         }
-        timeTable.start = sessionIdMap.hour + ":00";
-        timeTable.end = (sessionIdMap.hour + 1) + ":00";
+        timeTable.start = sessionIdMap.hour + ':00';
+        timeTable.end = (sessionIdMap.hour + 1) + ':00';
         return timeTable;
       }), ['sessionId']);
       this.timeTableList.reverse();
       console.log(this.timeTableList)
     }, error => {
-      console.log("fetching time table failed", error);
+      console.log('fetching time table failed', error);
     });
   }
+
+  getAttendence(sessionId) {
+    return this.dataService.getData('https://devcon.sunbirded.org/api/period/v3/attendance/' + sessionId, {
+      headers: {
+        'Content-Type': 'application/json',
+        // tslint:disable-next-line:max-line-length
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIyZWU4YTgxNDNiZWE0NDU4YjQxMjcyNTU5ZDBhNTczMiJ9.7m4mIUaiPwh_o9cvJuyZuGrOdkfh0Nm0E_25Cl21kxE'
+      }
+    }).pipe(map((res: any) => {
+      return res || [];
+    }));
+  }
+
+  getAssessmentScore(sessionId) {
+    return this.dataService.getData('https://devcon.sunbirded.org/api/period/v3/assessment/' + sessionId, {
+      headers: {
+        'Content-Type': 'application/json',
+        // tslint:disable-next-line:max-line-length
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIyZWU4YTgxNDNiZWE0NDU4YjQxMjcyNTU5ZDBhNTczMiJ9.7m4mIUaiPwh_o9cvJuyZuGrOdkfh0Nm0E_25Cl21kxE'
+      }
+    }).pipe(map((res: any) => {
+      return res || [];
+    }));
+  }
+
+  public refreshAttendee() {
+    this.state = 'rotated';
+    this.getAttendence(this.curentSessionId).subscribe(data => {
+      this.attendenceList = data;
+    });
+    setTimeout(() => {
+      this.state = 'default';
+    }, 1500);
+  }
+
+  public refreshAssessment() {
+    this.state2 = 'rotated';
+    this.getAssessmentScore(this.curentSessionId).subscribe(data => {
+      this.assessmentScore = data;
+    });
+    setTimeout(() => {
+      this.state2 = 'default';
+    }, 1500);
+  }
+
 }
