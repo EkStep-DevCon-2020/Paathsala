bab974f9f038b4a8c8035cf17b4efb9494f3c97a
anoophm <anoophm44@gmail.com>
2020-02-19 12:35:35 +0530

    issue SB-0000 feat: qr scanner

--- src/app/client/src/app/login/login.component.ts
+++ src/app/client/src/app/login/login.component.ts
@@ -1,10 +1,10 @@
 import { ConfigService } from '../config/config.service';
-import { TelemetryService } from '../telemetry/telemetry.service'
+import { TelemetryService } from '../telemetry/telemetry.service';
 import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { UUID } from 'angular2-uuid';
 import { catchError, map } from 'rxjs/operators';
 import { throwError, Observable } from 'rxjs';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 const STALL_ID = 'school';
 const IDEA_ID = 'staff-room';
@@ -23,8 +23,11 @@ export class LoginComponent implements OnInit {
   openSuccessModal = false;
   openErrorModal = false;
   captureImage = false;
+  visitorid = '';
+  name = '';
   qrCode = false;
   image: string;
+  camera;
   enteredQrCode: any;
   constraints = {
     video: {
@@ -35,8 +38,9 @@ export class LoginComponent implements OnInit {
   };
 
   constructor(private renderer: Renderer2,
-    public configService: ConfigService,
-    public telemetryServcie: TelemetryService, private router: Router) { }
+              public configService: ConfigService,
+              public telemetryServcie: TelemetryService,
+              public router: Router) { }
 
   ngOnInit() {
     this.telemetryServcie.initialize({
@@ -54,10 +58,10 @@ export class LoginComponent implements OnInit {
       "photo": "https://vignette.wikia.nocookie.net/jamescameronsavatar/images/5/5c/Avatar_Logo_revised.png/revision/latest?cb=20100207200600",
       "osid": "1-63c8bfe5-e5ca-4f3b-bb1a-46ac41f3b917"
     };
+    this.name = this.configService.userInfo.name;
     this.openSuccessModal = true;
   }
   startCamera() {
-    this.openErrorModal = false;
     if (!!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)) {
       navigator.mediaDevices.getUserMedia(this.constraints).then(this.attachVideo.bind(this)).catch(this.handleError);
     } else {
@@ -70,6 +74,7 @@ export class LoginComponent implements OnInit {
   }
 
   attachVideo(stream) {
+    this.camera = stream.getTracks()[0];
     this.renderer.setProperty(this.videoElement.nativeElement, 'srcObject', stream);
     this.renderer.listen(this.videoElement.nativeElement, 'play', (event) => {
       this.videoHeight = this.videoElement.nativeElement.videoHeight;
@@ -82,6 +87,7 @@ export class LoginComponent implements OnInit {
     this.renderer.setProperty(this.canvas.nativeElement, 'height', this.videoHeight);
     this.canvas.nativeElement.getContext('2d').drawImage(this.videoElement.nativeElement, 0, 0);
     this.image = this.canvas.nativeElement.toDataURL('image/png');
+    this.camera.stop();
     this.uploadImage();
   }
 
@@ -90,23 +96,25 @@ export class LoginComponent implements OnInit {
     const imageId = 'do_11295943688090419214';
     const imageName = `${id}.png`;
     fetch(this.image)
-      .then(res => res.blob())
-      .then(blob => {
-        const fd = new FormData();
-        const file = new File([blob], imageName);
-        fd.append('file', file);
-        const request = {
-          url: `private/content/v3/upload/${imageId}`,
-          data: fd
-        };
-        this.configService.post(request).pipe(catchError(err => {
-          const errInfo = { errorMsg: 'Image upload failed' };
-          return throwError(errInfo);
-        })).subscribe((response) => {
-          console.log('response ', response);
-          this.identifyFace(response);
-        });
+    .then(res => res.blob())
+    .then(blob => {
+      const fd = new FormData();
+      const file = new File([blob], imageName);
+      fd.append('file', file);
+      const request = {
+        url: `private/content/v3/upload/${imageId}`,
+        data: fd
+      };
+      this.configService.post(request).pipe(catchError(err => {
+        const errInfo = { errorMsg: 'Image upload failed' };
+        this.camera.stop();
+        return throwError(errInfo);
+
+      })).subscribe((response) => {
+        console.log('response ', response);
+        this.identifyFace(response);
       });
+    });
   }
 
   identifyFace(response) {
@@ -128,12 +136,59 @@ export class LoginComponent implements OnInit {
       this.telemetryServcie.visit(data);
       this.openSuccessModal = true;
       this.openErrorModal = false;
+      this.name = res.result.name;
       this.configService.userInfo = res.result;
-      console.log('-------response--------', res);
-    }, error => {
+      console.log('response ', res);
+    }, (err) => {
+      console.log('identifyFace err ', err);
+      this.openSuccessModal = false;
       this.openErrorModal = true;
-      console.log('-------error--------', error);
     });
+  }
+
+  getUserDtailsByVisitorId() {
+    if (this.visitorid) {
+      const request = {
+        url: `reg/search`,
+        header: {
+          'Content-Type': 'application/json'
+        },
+        data: {
+          request: {
+            entityType: ['Visitor'],
+            filters: {
+              code: {
+                eq : this.visitorid
+              }
+            }
+          }
+        }
+      };
+      this.configService.post(request).pipe().subscribe((res) => {
+        if (res.result.Visitor && res.result.Visitor.length) {
+          console.log('response ', res.result.Visitor[0]);
+          const data = {
+            profileId: res.result.Visitor[0].osid
+          };
+          this.configService.userInfo = res.result;
+          this.telemetryServcie.visit(data);
+          this.openSuccessModal = true;
+          this.openErrorModal = false;
+          this.name = res.result.Visitor[0].name;
+        } else if (this.visitorid === "000") {
+          this.handleQRCode();
+        } else {
+          console.log('user not found');
+          this.openSuccessModal = false;
+          this.openErrorModal = true;
+        }
+      }, (err) => {
+        console.log('identifyFace err ', err);
+        this.openSuccessModal = false;
+        this.openErrorModal = true;
+      });
+
+    }
   }
   navigate() {
     if(this.canvas && this.canvas.nativeElement){
