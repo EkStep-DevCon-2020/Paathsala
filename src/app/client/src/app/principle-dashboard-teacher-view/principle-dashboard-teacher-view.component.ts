c2b74fc8f1b5404a06792234cb664058b676a78b
christopher.fernandes <christyabcd.05@gmail.com>
2020-02-19 18:37:00 +0530

    Principal dashboard teacher view added

--- src/app/client/src/app/principle-dashboard-teacher-view/principle-dashboard-teacher-view.component.ts
+++ src/app/client/src/app/principle-dashboard-teacher-view/principle-dashboard-teacher-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../staff/service/data.service';
 import {SLIDE_UP_DOWN, FLYIN, APPEAR_DOWN, CARD_ANIMATION, APPEAR_SIDE} from '../theme/app-animations';
 
 @Component({
@@ -8,10 +10,41 @@ import {SLIDE_UP_DOWN, FLYIN, APPEAR_DOWN, CARD_ANIMATION, APPEAR_SIDE} from '..
   animations: [SLIDE_UP_DOWN, FLYIN, APPEAR_DOWN, CARD_ANIMATION, APPEAR_SIDE]
 })
 export class PrincipleDashboardTeacherViewComponent implements OnInit {
-
-  constructor() { }
+  public defaultProfileImg = './assets/images/default-profile.png';
+  public teacherList;
+  constructor(private dataService: DataService, public router: Router) { }
 
   ngOnInit() {
+    const body = {
+      "request": {
+          "filters": {
+            "objectType":"Teacher",
+            "identifier": ["T1", "T2","T3", "T4", "T5","T6"],
+            "status": []
+          }
+      }
+  };
+    this.dataService.post('https://devcon.sunbirded.org/action/composite/v3/search', body, { headers: {'Content-Type': 'application/json'}})
+    .subscribe((data: any) => {
+        console.log(data);
+        this.teacherList = data.result.Teacher;
+        if (this.teacherList.length > 0 ){
+          // this.teacherList[0].selected = true;
+          this.toggleSelected(this.teacherList[0]);
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  public toggleSelected(teacher) {
+    this.teacherList.map( t => {
+      t.selected = false;
+    });
+    console.log(teacher);
+    teacher.selected = !teacher.selected;
   }
 
 }
