e182f55069bdef6e0aec8c4448f2ec8a239341da
Devesh <mailme.devmehta@gmail.com>
2020-02-19 14:31:58 +0530

    Issue #SB-0000 fix:added api calls to populate dashboard data

--- src/app/client/src/app/staff/components/dashboard/dashboard.component.ts
+++ src/app/client/src/app/staff/components/dashboard/dashboard.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SLIDE_UP_DOWN, FLYIN, APPEAR_DOWN, CARD_ANIMATION, APPEAR_SIDE} from '../../../theme/app-animations';
+import {map} from 'rxjs/operators';
+import {DataService} from '../../service/data.service';
+import {Router, ActivatedRoute} from '@angular/router';
+import {ToasterService} from '@sunbird/shared';
+import {forkJoin} from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,10 +15,64 @@ import {SLIDE_UP_DOWN, FLYIN, APPEAR_DOWN, CARD_ANIMATION, APPEAR_SIDE} from '..
   ]
 })
 export class DashboardComponent implements OnInit {
+  teacherProfile: any;
+  queryParam: any;
+  timeTable: any;
 
-  constructor() { }
+  constructor(private dataService: DataService, public activatedRoute: ActivatedRoute,
+              public toasterService: ToasterService) {
+  }
 
   ngOnInit() {
+    this.fetchData(this.activatedRoute.snapshot.params.qrCode).subscribe(data => {
+      console.log(data);
+      this.teacherProfile = data;
+      this.getTimeTable(this.teacherProfile.sessionId).subscribe((periods: any) => {
+        console.log('timetable data', periods);
+        this.timeTable = periods;
+      }, error => {
+        this.toasterService.error('Error Loading data Please try again Later');
+        console.log('timetable fetching', error);
+      });
+    }, error => {
+      this.toasterService.error('Error Loading data Please try again Later');
+      console.log('fetchData error', error);
+    });
+  }
+
+  fetchData(qrCode) {
+    return this.dataService.getData('https://devcon.sunbirded.org/api/teacher/v3/user/profile/' + qrCode, {
+      headers: {
+        'Content-Type': 'application/json',
+        // tslint:disable-next-line:max-line-length
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIyZWU4YTgxNDNiZWE0NDU4YjQxMjcyNTU5ZDBhNTczMiJ9.7m4mIUaiPwh_o9cvJuyZuGrOdkfh0Nm0E_25Cl21kxE'
+      }
+    }).pipe(map(response => {
+      return response.result.teacher;
+    }));
+  }
+
+  getTimeTable(sessionId) {
+    const sessionStartTime = sessionId.slice(0, -2);
+    return this.dataService.post('https://devcon.sunbirded.org/action/composite/v3/search', {
+      'request': {
+        'filters': {
+          'objectType': 'Period',
+          'sessionId': {
+            'startsWith': sessionStartTime
+          },
+          'status': []
+        }
+      }
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        // tslint:disable-next-line:max-line-length
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIyZWU4YTgxNDNiZWE0NDU4YjQxMjcyNTU5ZDBhNTczMiJ9.7m4mIUaiPwh_o9cvJuyZuGrOdkfh0Nm0E_25Cl21kxE'
+      }
+    }).pipe(map((res: any) => {
+      return res.result.Period;
+    }));
   }
 
 }
